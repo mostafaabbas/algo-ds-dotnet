9d34420f24bf297cf990463c2366d696503fa667
Mostafa Abbas <mostafa.swcr@gmail.com>
2021-12-17 16:31:47 +0200

    hash table: set

---
+++ algo-ds-dotnet/algo-ds-dotnet/DataStructures/NewFolder/CustomHashTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo_ds_dotnet.DataStructures.NewFolder
+{
+    public class CustomHashTable
+    {
+        public CustomHashTable(int size = 53)
+        {
+            KeyMap = new List<List<string>>[size];
+        }
+
+        public List<List<string>>[] KeyMap { get; private set; }
+
+
+        private int Hash(string key)
+        {
+            var total = 0;
+            var WEIRD_PRIME = 31;
+            for (var i = 0; i < Math.Min(key.Length, 100); i++)
+            {
+                var c = key[i];
+                var value = (int)c - 96;
+                total = (total * WEIRD_PRIME + value) % KeyMap.Length;
+            }
+            return total;
+        }
+
+        public void Set(string key, string value) //separate chaining
+        {
+            var index = Hash(key);
+            var arr = KeyMap[index];
+            if (arr == null)
+                KeyMap[index] = new List<List<string>>();
+            KeyMap[index].Add(new List<string> { key, value });
+        }
+    }
+}
--- algo-ds-dotnet/algo-ds-dotnet/DataStructures/NewFolder/HashTables_Test.cs
+++ algo-ds-dotnet/algo-ds-dotnet/DataStructures/NewFolder/HashTables_Test.cs
@@ -11,17 +11,26 @@ namespace algo_ds_dotnet.DataStructures.NewFolder
     {
         public static void Run()
         {
-            var arrLen = 31;
-            Console.WriteLine("********* Naive_1 **********************");
-            Console.WriteLine($"pink: => {NaiveHashing.NaiveHash_1("pink", arrLen)}");
-            Console.WriteLine($"red: => {NaiveHashing.NaiveHash_1("red", arrLen)}");
-            Console.WriteLine($"blue: => {NaiveHashing.NaiveHash_1("blue", arrLen)}");
-            Console.WriteLine($"cyan: => {NaiveHashing.NaiveHash_1("cyan", arrLen)}");
-            Console.WriteLine("********* Naive_2 **********************");
-            Console.WriteLine($"pink: => {NaiveHashing.NaiveHash_2("pink", arrLen)}");
-            Console.WriteLine($"red: => {NaiveHashing.NaiveHash_2("red", arrLen)}");
-            Console.WriteLine($"blue: => {NaiveHashing.NaiveHash_2("blue", arrLen)}");
-            Console.WriteLine($"cyan: => {NaiveHashing.NaiveHash_2("cyan", arrLen)}");
+            //var arrLen = 31;
+            //Console.WriteLine("********* Naive_1 **********************");
+            //Console.WriteLine($"pink: => {NaiveHashing.NaiveHash_1("pink", arrLen)}");
+            //Console.WriteLine($"red: => {NaiveHashing.NaiveHash_1("red", arrLen)}");
+            //Console.WriteLine($"blue: => {NaiveHashing.NaiveHash_1("blue", arrLen)}");
+            //Console.WriteLine($"cyan: => {NaiveHashing.NaiveHash_1("cyan", arrLen)}");
+            //Console.WriteLine("********* Naive_2 **********************");
+            //Console.WriteLine($"pink: => {NaiveHashing.NaiveHash_2("pink", arrLen)}");
+            //Console.WriteLine($"red: => {NaiveHashing.NaiveHash_2("red", arrLen)}");
+            //Console.WriteLine($"blue: => {NaiveHashing.NaiveHash_2("blue", arrLen)}");
+            //Console.WriteLine($"cyan: => {NaiveHashing.NaiveHash_2("cyan", arrLen)}");
+
+            CustomHashTable ht = new CustomHashTable(9);
+            ht.Set("red", "#FF0000");
+            ht.Set("blue", "#0000FF");
+            ht.Set("yellow", "#FFFF00");
+            ht.Set("black", "#000000");
+            ht.Set("white", "#FFFFFF");
+
+
         }
 
 
